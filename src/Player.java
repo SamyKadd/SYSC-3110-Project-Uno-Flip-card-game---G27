7c99ee112698fe49f828e2047b8d1a6a3b500211
Joodi <joodialasaad@cmail.carleton.ca>
2025-10-20 16:19:33 -0400

    Add attributes to Player class (name, score, hand) with setters, getters, and displayHand() method

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,22 @@
 public class Player {
+    private String name;
+    private int score;
+    private Hand hand;
+
+    public Player(String name) {
+        this.name = name;
+        hand = new Hand();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void displayHand(){
+        //System.out.println(hand.toString());
+    }
+
 }
