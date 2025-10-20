48eb89e6cb09d8532a99cfc1d8a59e20e7ebd43b
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-10-20 17:40:58 -0400

    Similarely, started a rough skeleton code for Hand class.

--- src/Hand.java
+++ src/Hand.java
@@ -2,5 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-    
+    private List<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    // public Card removeCard(int index) {
+    // }
+
+    // Will have identical logic to remove, but obviously just getting instead of removing
+    // public Card getCard(int index) {
+    // }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    // @Override
+    // public String toString() {
+    // }
 }
