e45f1112956b8c776afec0b159255052102f6c36
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-10-20 17:13:18 -0400

    Started a rough skeleton code for Card class.

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,31 @@
 public class Card {
+    private String color;
+    private String value;
+    // Variable for cards that are +4, +2, etc...
+    // private boolean isActionCard;
 
+    public Card(String color, String value) {
+        this.color = color;
+        this.value = value;
+        // this.isActionCard = ___; // Logic to determine if it's an action card
+    }
+
+    // Need to add the function that will check if it is an Action Card here
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    // public boolean isActionCard() {
+    //     return isActionCard;
+    // }
+
+    @Override
+    public String toString() {
+        return color + " " + value;
+    }
 }
