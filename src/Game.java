1a643fe02bd1ace449bdadee5521108730422c73
Joodi <joodialasaad@cmail.carleton.ca>
2025-10-20 23:02:31 -0400

    Started a rough skeleton code for Game Class

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,18 @@ import java.util.*;
 
 public class Game {
     private List<Player> players;
+
+    public Game() {
+        players = new ArrayList<>();
+    }
+
+    public void addPlayer(Player p){
+        players.add(p);
+    }
+    public void removePlayer(Player p){
+        players.remove(p);
+    }
+    public Player getPlayer(int index){
+        return players.get(index);
+    }
 }
