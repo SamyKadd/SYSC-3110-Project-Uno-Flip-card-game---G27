96c26b310275c678b937d46f774c027f29203b37
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-10-21 17:21:40 -0400

    Declared function to initialize deck (not yet implemented) and added conditional checks to make sure we have 2-4 players.

--- src/Game.java
+++ src/Game.java
@@ -7,9 +7,19 @@ public class Game {
     public Game() {
         players = new ArrayList<>();
         deck = new ArrayList<>();
+        // Initialize deck with cards
+        // initializeDeck();
     }
 
+    // private void initializeDeck() {
+    // }
+
     public void addPlayer(Player p){
+        
+        if (players.size() >= 4) {
+            System.out.println("Cannot add more players. Maximum is 4 players.");
+            return;
+        }
         players.add(p);
     }
     public void removePlayer(Player p){
@@ -20,6 +30,17 @@ public class Game {
     }
 
     public void startGame(){
+        
+        if (players.size() < 2) {
+            System.out.println("Need at least 2 players to start the game.");
+            return;
+        }
+        
+        if (players.size() > 4) {
+            System.out.println("Maximum 4 players allowed.");
+            return;
+        }
+        
         for (Player player: players){
             player.getHand().StartCards(deck);
             System.out.println(player.getName() + " hand:");
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        new Game();
+        Game game = new Game();
+        
+        // We can modify this, just creating two mock players to test
+        game.addPlayer(new Player("Player 1"));
+        game.addPlayer(new Player("Player 2"));
+        
+        game.startGame();
+
     }
 }
