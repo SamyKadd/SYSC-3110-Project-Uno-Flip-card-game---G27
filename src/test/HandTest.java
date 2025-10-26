178cffac9063b487dac65dccf4f5faba1c2a40a8
Joodi <joodialasaad@cmail.carleton.ca>
2025-10-26 15:05:22 -0400

    Add test to verify startCards removes all cards from the deck

--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import java.util.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HandTest {
@@ -46,4 +50,25 @@ public class HandTest {
         assertEquals(card2, hand.getCard(0));
     }
 
+    @Test
+    public void testStartCard() {
+        Hand hand = new Hand();
+        ArrayList<Card> deck = new ArrayList<>();
+        Card card = new Card(Card.Color.GREEN, Card.Value.EIGHT);
+        deck.add(card);
+        Card card2 = new Card(Card.Color.RED, Card.Value.SKIP);
+        deck.add(card2);
+        Card card3 = new Card(Card.Color.BLUE, Card.Value.SIX);
+        deck.add(card3);
+        Card card4 = new Card(Card.Color.GREEN, Card.Value.ONE);
+        deck.add(card4);
+        Card card5 = new Card(Card.Color.YELLOW, Card.Value.THREE);
+        deck.add(card5);
+        Card card6 = new Card(Card.Color.GREEN, Card.Value.TWO);
+        deck.add(card6);
+        Card card7 = new Card(Card.Color.RED, Card.Value.EIGHT);
+        deck.add(card7);
+        hand.startCards(deck);
+        assertEquals(0, deck.size());
+    }
 }
