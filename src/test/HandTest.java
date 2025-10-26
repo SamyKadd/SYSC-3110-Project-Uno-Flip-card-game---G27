d1f9bc89cbb251676ae5641a3ff80f9479be7674
Joodi <joodialasaad@cmail.carleton.ca>
2025-10-26 13:53:58 -0400

    Add test confirming Hand initializes with size 0

--- src/test/HandTest.java
+++ src/test/HandTest.java
@@ -1,1 +1,12 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class HandTest {
+    @Test
+    public void testHand() {
+        Hand hand = new Hand();
+        assertTrue(hand.getSize() == 0);
+    }
+}
