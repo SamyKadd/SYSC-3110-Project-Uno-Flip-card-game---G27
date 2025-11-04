2553b1dc77ffdd7a7ccff75f131982a751216f13
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-11-04 17:45:47 -0500

    Initial file declaration, this will be the dialog box that pops up to prompt for the number of players. Will have to integrate with MVC/game later

---
+++ src/PlayerSelectionDialog.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Dialog for selecting the number of players at game start.
+ * Allows selection of 2-4 players.
+ *
+ * @author G27
+ * @version 1.0
+ */
+public class PlayerSelectionDialog extends JDialog {
+    
+    private int numberOfPlayers;
+    private boolean confirmed;
+    private JComboBox<Integer> playerCountComboBox;
+    
+    
+}
