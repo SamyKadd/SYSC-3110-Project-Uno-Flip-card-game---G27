b3d741cc047bcc7f548ac714d5b49ed6bccca262
Samy Kaddour <samykaddour27@gmail.com>
2025-11-08 12:55:36 -0500

    Adding this class as a renderer

---
+++ src/GameState.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameState {
+    public String curPlayerName;
+    public List<Card> curHand = new ArrayList<>();
+    public Card topCard;
+
+    public String statusMessage = "";
+
+    //Using the following to enable or disable controls in view
+    public boolean canPlay;
+    public boolean canDraw;
+    public boolean canNext;
+    public boolean needsWildColor;
+
+}
