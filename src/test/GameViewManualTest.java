7603f8f30095b2afb090a015542d3f3315bdd6c1
Mahdi Bouakline <bouaklinemahdi@gmail.com>
2025-11-10 18:48:47 -0500

    Forgot renaming a file deletes it, added it back.

---
+++ src/test/GameViewManualTest.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+
+/**
+ * Simple test to view the GameView GUI.
+ * Creates sample data to see how the interface looks.
+ *
+ * @author G27
+ * @version 2.0
+ */
+// THIS WAS NOT REQUIRED, I just made it to test the appearance of the GUI
+// as I was working on it/modifying it.
+public class GameViewManualTest {
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            System.out.println("Creating GameView...");
+            
+            // Create the view
+            GameView view = new GameView();
+            
+            // Create test data
+            Hand testHand = new Hand();
+            testHand.addCard(new Card(Card.Color.RED, Card.Value.THREE));
+            testHand.addCard(new Card(Card.Color.BLUE, Card.Value.FIVE));
+            testHand.addCard(new Card(Card.Color.GREEN, Card.Value.REVERSE));
+            testHand.addCard(new Card(Card.Color.YELLOW, Card.Value.DRAW_ONE));
+            testHand.addCard(new Card(Card.Color.RED, Card.Value.SKIP));
+            testHand.addCard(new Card(null, Card.Value.WILD));
+            testHand.addCard(new Card(Card.Color.BLUE, Card.Value.SEVEN));
+            
+            // Display the hand
+            view.displayHand(testHand);
+            
+            // Set top card
+            Card topCard = new Card(Card.Color.GREEN, Card.Value.TWO);
+            view.updateTopCard(topCard);
+            
+            // Update other elements
+            view.updateCurrentPlayer("Test Player");
+            view.updateStatusMessage("Testing the view");
+            
+            // Create test players for scoreboard
+            java.util.List<Player> players = new java.util.ArrayList<>();
+            players.add(new Player("Player 1"));
+            players.add(new Player("Player 2"));
+            view.updateScoreboard(players);
+            
+            System.out.println("GameView created! Window should be visible.");
+            
+            // Add test listeners
+            view.getDrawCardButton().addActionListener(e -> {
+                System.out.println("Draw Card button works!");
+            });
+            
+            view.getNextPlayerButton().addActionListener(e -> {
+                System.out.println("Next Player button works!");
+            });
+            
+            for (JButton btn : view.getCardButtons()) {
+                btn.addActionListener(e -> {
+                    System.out.println("Card button " + btn.getActionCommand() + " works!");
+                });
+            }
+        });
+    }
+}
